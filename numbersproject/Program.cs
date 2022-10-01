78c3aa35ad17c5cf4982388d2c4efb5db43b74a0
Amad <smart_kazmi@hotmail.com>
2022-10-02 01:38:55 +0200

    Add project files.

---
+++ numbersproject/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("numbers project");
+
+var a = 12;
+var b = 5;
+Console.WriteLine("please enter the first number");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("please enter the second number");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Addition:");
+Console.WriteLine(a+ b);
+//additon of 12+5=17
+Console.WriteLine("Subtraction:");
+Console.WriteLine(a - b);
+//subtraction 12-5=7
+Console.WriteLine("Sultipication:");
+Console.WriteLine(a *b);
+//multiply 12*5=60
+Console.WriteLine("Division:");
+Console.WriteLine(12 / 5.0f);
+//for getting an exact divison value we make that float by adding .0 =2.4
+Console.WriteLine("Modulus:");
+Console.WriteLine(a % b);
+//moduls=2
+
+
+
+
+
+
+
+
+
+
+
+
+
